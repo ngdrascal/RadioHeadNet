6a7d9fb1edb12cc1d65bbc99d62ab573ddd0b9c2
Rick Dodd <rick.dodd@comcast.net>
2025-06-02 17:37:17 -0500

    incremental updates

---
+++ Examples/nanoFramework/Rf95/Rf95ServerNf/Program.cs
@@ -0,0 +1,15 @@
+using System.Diagnostics;
+using System.Threading;
+
+namespace RadioHead.Examples.Rf95ServerNf
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Debug.WriteLine("Hello from nanoFramework!");
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
